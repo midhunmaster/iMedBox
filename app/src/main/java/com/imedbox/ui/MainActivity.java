05837643415218cfb4245e76017d434c3ef0e13e
Midhun Mathew <midhun.mathew@philips.com>
2019-03-17 23:23:21 +0530

    Implemented Tabs, one each for Insulin and Tablets

--- app/src/main/java/com/imedbox/ui/MainActivity.java
+++ app/src/main/java/com/imedbox/ui/MainActivity.java
@@ -1,15 +1,31 @@
 package com.imedbox.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.imedbox.R;
+import com.imedbox.ui.Fragments.InsulinFragment;
+import com.imedbox.ui.Fragments.TabletsFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new TabletsFragment(), "Tablets");
+        adapter.addFragment(new InsulinFragment(), "Insulin");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
